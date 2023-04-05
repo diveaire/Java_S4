a4ec29d541ef67899299dee00955a6fe4815d98d
diveaire <n.auvray.na@gmail.com>
2023-04-05 14:53:49 +0200

    Base du projet

---
+++ src/Graphe/Graphe.java
@@ -0,0 +1,9 @@
+package Graphe;
+
+import java.awt.*;
+import java.io.Serializable;
+import javax.swing.*;
+
+class Graphe implements Serializable {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import Affichage.Windows;
+
+public class Main {
+    public static void main(String[] args) {
+        Windows G = new Windows("Graphe");
+    }
+}
