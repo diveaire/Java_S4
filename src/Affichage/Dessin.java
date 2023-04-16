e821e63af865cc963ebce3878e1d95d4851e4773
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-16 18:02:41 +0200

    Add files via upload

--- src/Affichage/Dessin.java
+++ src/Affichage/Dessin.java
@@ -5,10 +5,11 @@ public class Dessin extends JPanel{
     private Graphe g;
     public Dessin(){
         this.g=new Graphe();
-        this.g.addSommet(new Rond(30,300,20));
-        this.g.addSommet(new Rond(20,20,20));
-        this.g.addSommet(new Rond(450,30,20));
-        this.g.addSommet(new Rond(600,95,20));
+        this.g.addSommet(new Rond("A",0,0,20));
+        this.g.addSommet(new Carre("B",30,30,20));
+        this.g.addSommet(new Triangle("C",50,50,20,Color.RED));
+        this.g.addSommet(new Rond("D",430,430,20,Color.RED));
+        this.g.addArc(new Rond("A",0,0,20),new Carre("B",30,30,20));
     }
     public void paint(Graphics g){
         this.g.paint(g);
