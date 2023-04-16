6e6200eb5ab85806bac04bd52e0cc55d19f2b94e
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-16 12:38:02 +0200

    Add files via upload

--- src/Affichage/Dessin.java
+++ src/Affichage/Dessin.java
@@ -1,4 +1,16 @@
-package Affichage;
+import java.awt.*;
+import javax.swing.*;
 
-public class Dessin {
+public class Dessin extends JPanel{
+    private Graphe g;
+    public Dessin(){
+        this.g=new Graphe();
+        this.g.addSommet(new Rond(30,300,20));
+        this.g.addSommet(new Rond(20,20,20));
+        this.g.addSommet(new Rond(450,30,20));
+        this.g.addSommet(new Rond(600,95,20));
+    }
+    public void paint(Graphics g){
+        this.g.paint(g);
+    }
 }
--- src/Affichage/Windows.java
+++ src/Affichage/Windows.java
@@ -1,76 +1,12 @@
-package Affichage;
-
 import java.awt.*;
 import javax.swing.*;
 
 public class Windows extends JFrame{
-	public Windows(String name){
+    private Dessin d;
+	Windows(String name){
 		super(name);
-		
-		//toolBox
-		JToolBar tb = new JToolBar("ToolBox", 1);
-		tb.setFloatable(true);
-		tb.setRollover(true);
-
-        ButtonGroup btnGp = new ButtonGroup();
-        JRadioButton rBtn1 = new JRadioButton("Rond");
-        JRadioButton rBtn2 = new JRadioButton("Carré");
-        JRadioButton rBtn3 = new JRadioButton("Triangle");
-        btnGp.add( rBtn1 );
-        btnGp.add( rBtn2 );
-        btnGp.add( rBtn3 );
-        tb.add((Action) btnGp);
-        JButton btnNew = new JButton( new ImageIcon( "icons/new.png") );
-        btnNew.setToolTipText( "New File (CTRL+N)" );
-        tb.add( btnNew );
-
-        JButton btnSave = new JButton( new ImageIcon( "icons/save.png" ) );
-        btnSave.setToolTipText( "Save (CTRL+S)" );
-        tb.add( btnSave );
-
-        JButton btnSaveAs = new JButton( new ImageIcon( "icons/save_as.png" ) );
-        btnSaveAs.setToolTipText( "Save As..." );
-        tb.add( btnSaveAs );
-
-        tb.addSeparator();
-
-        JButton btnCopy = new JButton( new ImageIcon( "icons/copy.png") );
-        btnCopy.setToolTipText( "Copy (CTRL+C)" );
-        tb.add( btnCopy );
-
-        JButton btnCut = new JButton( new ImageIcon( "icons/cut.png") );
-        btnCut.setToolTipText( "Cut (CTRL+X)" );
-        tb.add( btnCut );
-
-        JButton btnPaste = new JButton( new ImageIcon( "icons/paste.png") );
-        btnPaste.setToolTipText( "Paste (CTRL+V)" );
-        tb.add( btnPaste );
-
-        tb.addSeparator();
-
-        JButton btnExit = new JButton( new ImageIcon( "icons/exit.png") );
-        btnExit.setToolTipText( "Exit (ALT+F4)" );
-        tb.add( btnExit );
-
-        //tb.addSeparator();
-        // Autres types de composants graphiques
-        //tb.add( new JCheckBox( "Check me" ) );
-        //tb.add( new JTextField( "Edit me" ) );
-		
-		//panel
-		JScrollPane aff = new JScrollPane();
-		
-		//Propriétés
-		JLabel prop = new JLabel("Panel PROP");
-		
-		
-		JSplitPane panel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,aff,prop);
-		panel.setDividerSize(20);
-		panel.setDividerLocation(1200);
-		
-		this.add(panel);
-		this.getContentPane().add(tb, BorderLayout.WEST);
-		
+		this.d=new Dessin();
+        this.add(d);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(1980,1080);
 		this.setVisible(true);
@@ -79,7 +15,4 @@ public class Windows extends JFrame{
 	public static void main(String[] argv){
         Windows G = new Windows("Graphe");
 	}
-	
-	
-	
 }
