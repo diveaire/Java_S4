bd3ffe912862c7f4d4f41aa44d5db1eb1d4c0ece
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-16 12:37:43 +0200

    Add files via upload

--- src/Graphe/Graphe.java
+++ src/Graphe/Graphe.java
@@ -1,9 +1,19 @@
-package Graphe;
-
+import java.util.*;
 import java.awt.*;
 import java.io.Serializable;
 import javax.swing.*;
 
 class Graphe implements Serializable {
-
+    private ArrayList<Sommet> listSom;
+    public Graphe(){
+        this.listSom=new ArrayList<Sommet>();
+    }
+    public void addSommet(Sommet s){
+        this.listSom.add(s);
+    }
+    public void paint(Graphics g){
+        for(Sommet s : this.listSom){
+            s.paint(g);
+        }
+    }
 }
