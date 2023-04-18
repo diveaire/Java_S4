0f4f661b29a3f0b8e079a0d5be093c0cc4a4648e
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-18 18:52:50 +0200

    Add files via upload

--- src/Affichage/Dessin.java
+++ src/Affichage/Dessin.java
@@ -1,17 +1,45 @@
 import java.awt.*;
+import java.awt.event.*;  
 import javax.swing.*;
-
-public class Dessin extends JPanel{
+public class Dessin extends JPanel implements MouseListener{
     private Graphe g;
     public Dessin(){
         this.g=new Graphe();
-        this.g.addSommet(new Rond("A",0,0,20));
+        addMouseListener(this);
+        this.g.addSommet(new Rond("A",10,10,20));
         this.g.addSommet(new Carre("B",30,30,20));
         this.g.addSommet(new Triangle("C",50,50,20,Color.RED));
         this.g.addSommet(new Rond("D",430,430,20,Color.RED));
-        this.g.addArc(new Rond("A",0,0,20),new Carre("B",30,30,20));
+        this.g.addArc(new Rond("A",10,10,20),new Carre("B",30,30,20));
+        this.g.addArc(new Carre("B",30,30,20),new Triangle("C",50,50,20,Color.RED));
+        this.g.addArc(new Triangle("C",50,50,20,Color.RED),new Rond("D",430,430,20,Color.RED));
     }
-    public void paint(Graphics g){
+    public Graphe getGraphe(){
+        return this.g;
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); 
         this.g.paint(g);
     }
+    public void mouseClicked(MouseEvent e){
+        if(e.getButton()==MouseEvent.BUTTON1){
+            if(g.isSommetInList(new Rond("",e.getX(),e.getY(),20))){
+                Sommet x=this.g.getSommet(new Rond("",e.getX(),e.getY(),20));
+                x.setCouleur(Color.GREEN);
+                this.repaint();
+            }
+            else{
+                this.g.addSommet(new Rond("A",e.getX()-10,e.getY()-10,20));
+                this.repaint();
+            }
+        }
+        else if((e.getButton()==MouseEvent.BUTTON3)&&(g.isSommetInList(new Rond("",e.getX(),e.getY(),20)))){
+            this.g.delSommet(new Rond("A",e.getX(),e.getY(),20));
+            this.repaint();
+        }
+    }
+    public void mouseEntered(MouseEvent e) {}  
+    public void mouseExited(MouseEvent e) {}  
+    public void mousePressed(MouseEvent e) {}  
+    public void mouseReleased(MouseEvent e) {}  
 }
--- src/Affichage/Windows.java
+++ src/Affichage/Windows.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Windows extends JFrame{
-    private Dessin d;
+    protected Dessin d;
 	Windows(String name){
 		super(name);
 		this.d=new Dessin();
@@ -11,8 +11,9 @@ public class Windows extends JFrame{
 		this.setSize(1980,1080);
 		this.setVisible(true);
 	}
-
 	public static void main(String[] argv){
-        Windows G = new Windows("Graphe");
+        Windows g = new Windows("Graphe");
+        Sommet a=g.d.getGraphe().getSommet(new Rond("A",10,10,20));
+        Sommet b=g.d.getGraphe().getSommet(new Carre("B",30,30,20));
 	}
 }
