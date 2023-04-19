05c9595fb7e9570c58042f85f28d4db2cbd3a813
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-19 23:30:26 +0200

    Add files via upload

--- src/Affichage/Dessin.java
+++ src/Affichage/Dessin.java
@@ -2,29 +2,23 @@ import java.awt.*;
 import java.awt.event.*;  
 import javax.swing.*;
 public class Dessin extends JPanel implements MouseListener{
-    private Graphe g;
+    private Graphe graphe;
     private int size;
     private String type;
-    private Sommet selSom,pselSom;
+    private Sommet selSom,pselSom,movedSom;
     private Arc selArc;
     public Dessin(){
-        this.g=new Graphe();
+        this.graphe=new Graphe();
         this.size=20;
         this.type="Rond";
         this.selSom=null;
         this.pselSom=null;
+        this.movedSom=null;
         this.selArc=null;
         addMouseListener(this);
-        this.g.addSommet(new Rond("A",10,10,20));
-        this.g.addSommet(new Carre("B",30,30,20));
-        this.g.addSommet(new Triangle("C",50,50,20,Color.BLUE));
-        this.g.addSommet(new Rond("D",430,430,20,Color.RED));
-        this.g.addArc(new Rond("A",10,10,20),new Carre("B",30,30,20));
-        this.g.addArc(new Carre("B",30,30,20),new Triangle("C",50,50,20,Color.RED));
-        this.g.addArc(new Triangle("C",50,50,20,Color.RED),new Rond("D",430,430,20,Color.RED));
     }
     public Graphe getGraphe(){
-        return this.g;
+        return this.graphe;
     }
     public String getType(){
         return this.type;
@@ -44,17 +38,20 @@ public class Dessin extends JPanel implements MouseListener{
     public Sommet getPrevSelSom(){
         return this.pselSom;
     }
+    public Sommet getMovedSom(){
+        return this.movedSom;
+    }
     public Arc getSelArc(){
         return this.selArc;
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g); 
-        this.g.paint(g);
+        this.graphe.paint(g);
     }
     public void mouseClicked(MouseEvent e){
         if(e.getButton()==MouseEvent.BUTTON1){
-            if(g.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
-                Sommet pointed=this.g.getSommet(new Rond("",e.getX(),e.getY(),this.size));
+            if(this.graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
+                Sommet pointed=this.graphe.getSommet(new Rond("",e.getX(),e.getY(),this.size));
                 if(this.selSom==null){
                     this.selSom=pointed;
                     pointed.setCouleur(Color.GREEN);
@@ -69,50 +66,104 @@ public class Dessin extends JPanel implements MouseListener{
                         this.selSom=null;
                     }
                     pointed.setCouleur(Color.BLACK);
+                    if (this.selArc!=null){
+                        this.selArc.setCouleur(Color.BLACK);
+                    }
+                    this.movedSom=null;
                     this.repaint();
                 }
                 else if(this.pselSom==null){
                     this.pselSom=pointed;
-                    g.addArc(this.selSom,this.pselSom);
+                    this.graphe.addArc(this.selSom,this.pselSom);
                     pointed.setCouleur(Color.GREEN);
                     this.repaint();
                 }
                 else if(this.pselSom.equals(pointed)){
                     this.pselSom=null;
                     pointed.setCouleur(Color.BLACK);
+                    if (this.selArc!=null){
+                        this.selArc.setCouleur(Color.BLACK);
+                    }
+                    this.movedSom=null;
                     this.repaint();
                 }
                 else{
                     this.selSom.setCouleur(Color.BLACK);
                     this.selSom=this.pselSom;
                     this.pselSom=pointed;
-                    g.addArc(this.selSom,this.pselSom);
+                    this.graphe.addArc(this.selSom,this.pselSom);
                     pointed.setCouleur(Color.GREEN);
+                    if (this.selArc!=null){
+                        this.selArc.setCouleur(Color.BLACK);
+                    }
+                    this.repaint();
+                }
+                if (this.selSom!=null && this.pselSom!=null && this.graphe.isArcInList(new Arc(this.selSom,this.pselSom))){
+                    this.selArc=this.graphe.getArc(new Arc(this.selSom,this.pselSom));
+                    this.selArc.setCouleur(Color.GREEN);
                     this.repaint();
                 }
             }
             else{
                 if(this.type=="Rond"){
-                    this.g.addSommet(new Rond("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
+                    this.graphe.addSommet(new Rond("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
                     this.repaint();
                 }
                 else if(this.type=="Carre"){
-                    this.g.addSommet(new Carre("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
+                    this.graphe.addSommet(new Carre("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
                     this.repaint();
                 }
                 else if(this.type=="Triangle"){
-                    this.g.addSommet(new Triangle("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
+                    this.graphe.addSommet(new Triangle("",e.getX()-this.size/2,e.getY()-this.size/2,this.size));
                     this.repaint();
                 }     
             }
         }
-        else if((e.getButton()==MouseEvent.BUTTON3)&&(g.isSommetInList(new Rond("",e.getX(),e.getY(),this.size)))){
-            this.g.delSommet(new Rond("",e.getX(),e.getY(),this.size));
+        else if((e.getButton()==MouseEvent.BUTTON3)&&(graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size)))){
+            Sommet pointed=this.graphe.getSommet(new Rond("",e.getX(),e.getY(),this.size));
+            if(this.selSom!=null && this.selSom.equals(pointed)){
+                if(this.pselSom!=null){
+                    this.selSom=this.pselSom;
+                    this.pselSom=null;
+                }
+                else{
+                    this.selSom=null;
+                }
+                if (this.selArc!=null){
+                    this.selArc.setCouleur(Color.BLACK);
+                }
+            }
+            else if(this.pselSom!=null && this.pselSom.equals(pointed)){
+                this.pselSom=null;
+                if (this.selArc!=null){
+                    this.selArc.setCouleur(Color.BLACK);
+                }
+            }
+            this.graphe.delSommet(pointed);
             this.repaint();
         }
     }
+    public void mousePressed(MouseEvent e){
+        if(e.getButton()==MouseEvent.BUTTON1){
+            if(this.graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
+                if(this.movedSom==null && this.selSom==null && this.pselSom==null){
+                    this.movedSom=this.graphe.getSommet(new Rond("",e.getX(),e.getY(),this.size));
+                }
+            }
+        }
+    }  
+    public void mouseReleased(MouseEvent e){
+        if(e.getButton()==MouseEvent.BUTTON1){
+            if(!this.graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
+                if(this.movedSom!=null && this.selSom==null && this.pselSom==null){
+                    this.movedSom.setX(e.getX()-this.movedSom.getLenght()/2);
+                    this.movedSom.setY(e.getY()-this.movedSom.getLenght()/2);
+                    this.movedSom=null;
+                    this.repaint();
+                }
+            }
+        }
+    }  
     public void mouseEntered(MouseEvent e) {}  
     public void mouseExited(MouseEvent e) {}  
-    public void mousePressed(MouseEvent e) {}  
-    public void mouseReleased(MouseEvent e) {}  
 }
--- src/Affichage/Windows.java
+++ src/Affichage/Windows.java
@@ -5,16 +5,53 @@ import java.awt.event.*;
 public class Windows extends JFrame implements ActionListener{
     protected Dessin d;
 	protected JToolBar toolBar;
+	protected JPanel uperBar;
 	Windows(String name){
 		super(name);
 		this.setLayout(new BorderLayout());
+
 		/*Initialisation de notre panel*/
 		this.d=new Dessin();
+
+		/*Création de la barre de menu */
+		this.uperBar=new JPanel();
+		this.uperBar.setBackground(Color.CYAN);
+		this.uperBar.setForeground(Color.BLACK);
+		Box uperBarBox=new Box(BoxLayout.X_AXIS);
+		JButton btnNew = new JButton( new ImageIcon( "icons/new.png") );
+        btnNew.setToolTipText( "New File (CTRL+N)" );
+        uperBarBox.add( btnNew );
+
+        JButton btnSave = new JButton( new ImageIcon( "icons/save.png" ) );
+        btnSave.setToolTipText( "Save (CTRL+S)" );
+        uperBarBox.add( btnSave );
+
+        JButton btnSaveAs = new JButton( new ImageIcon( "icons/save_as.png" ) );
+        btnSaveAs.setToolTipText( "Save As..." );
+        uperBarBox.add( btnSaveAs );
+
+        JButton btnCopy = new JButton( new ImageIcon( "icons/copy.png") );
+        btnCopy.setToolTipText( "Copy (CTRL+C)" );
+        uperBarBox.add( btnCopy );
+
+        JButton btnCut = new JButton( new ImageIcon( "icons/cut.png") );
+        btnCut.setToolTipText( "Cut (CTRL+X)" );
+        uperBarBox.add( btnCut );
+
+        JButton btnPaste = new JButton( new ImageIcon( "icons/paste.png") );
+        btnPaste.setToolTipText( "Paste (CTRL+V)" );
+
+        JButton btnExit = new JButton( new ImageIcon( "icons/exit.png") );
+        btnExit.setToolTipText( "Exit (ALT+F4)" );
+        uperBarBox.add( btnExit );
+		this.uperBar.add(uperBarBox);
+
 		/*Création de la barre d'outil*/
 		this.toolBar=new JToolBar("Propriétés");
 		this.toolBar.setFloatable(true);
 		this.toolBar.setRollover(true);
 		this.toolBar.setBackground(Color.lightGray);
+
 		/*Création des boutons permettant de sélectioner le type de sommet */
 		ButtonGroup typeSommet = new ButtonGroup();
 		JRadioButton typeRond = new JRadioButton("Rond");
@@ -32,9 +69,11 @@ public class Windows extends JFrame implements ActionListener{
 		this.toolBar.add(typeRond);
 		this.toolBar.add(typeCarre);
 		this.toolBar.add(typeTriangle);
+
 		/*Placement des composants*/
         this.add(d,BorderLayout.CENTER);
 		this.add(toolBar,BorderLayout.WEST);
+		this.add(uperBar,BorderLayout.NORTH);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(1980,1080);
 		this.setVisible(true);
@@ -47,7 +86,5 @@ public class Windows extends JFrame implements ActionListener{
     }
 	public static void main(String[] argv){
         Windows g = new Windows("Graphe");
-        Sommet a=g.d.getGraphe().getSommet(new Rond("A",10,10,20));
-        Sommet b=g.d.getGraphe().getSommet(new Carre("B",30,30,20));
 	}
 }
