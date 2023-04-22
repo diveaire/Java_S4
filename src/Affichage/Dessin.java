e729e02e81d7875a9c2fd78413c1650ae1d56396
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-22 18:28:29 +0200

    Add files via upload

--- src/Affichage/Dessin.java
+++ src/Affichage/Dessin.java
@@ -103,7 +103,6 @@ public class Dessin extends JPanel implements MouseListener{
                         this.selArc.setCouleur(Color.BLACK);
                         this.selArc=null;
                     }
-                    this.movedSom=null;
                     this.repaint();
                 }
                 else if(this.pselSom==null){
@@ -119,7 +118,6 @@ public class Dessin extends JPanel implements MouseListener{
                         this.selArc.setCouleur(Color.BLACK);
                         this.selArc=null;
                     }
-                    this.movedSom=null;
                     this.repaint();
                 }
                 else{
@@ -139,6 +137,7 @@ public class Dessin extends JPanel implements MouseListener{
                     this.selArc.setCouleur(Color.GREEN);
                     this.repaint();
                 }
+                this.movedSom=null;
             }
             else{
                 if(this.type!="Aucun"){
@@ -207,7 +206,7 @@ public class Dessin extends JPanel implements MouseListener{
     public void mousePressed(MouseEvent e){
         if(e.getButton()==MouseEvent.BUTTON1){
             if(this.graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
-                if(this.movedSom==null && this.selSom==null && this.pselSom==null){
+                if(this.movedSom==null){
                     this.movedSom=this.graphe.getSommet(new Rond("",e.getX(),e.getY(),this.size));
                 }
             }
@@ -216,7 +215,7 @@ public class Dessin extends JPanel implements MouseListener{
     public void mouseReleased(MouseEvent e){
         if(e.getButton()==MouseEvent.BUTTON1){
             if(!this.graphe.isSommetInList(new Rond("",e.getX(),e.getY(),this.size))){
-                if(this.movedSom!=null && this.selSom==null && this.pselSom==null){
+                if(this.movedSom!=null){
                     this.movedSom.setX(e.getX()-this.movedSom.getLenght()/2);
                     this.movedSom.setY(e.getY()-this.movedSom.getLenght()/2);
                     this.movedSom=null;
