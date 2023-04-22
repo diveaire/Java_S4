1fa63b5b6c3a3876517448840316c5321f7a4e1e
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-22 17:13:16 +0200

    Add files via upload

--- src/Affichage/BarreOutils.java
+++ src/Affichage/BarreOutils.java
@@ -8,22 +8,27 @@ public class BarreOutils extends JToolBar implements ActionListener{
         super(nom);
         this.d=draw;
         ButtonGroup typeSommet = new ButtonGroup();
+        JRadioButton typeAucun = new JRadioButton("Aucun");
 		JRadioButton typeRond = new JRadioButton("Rond");
         JRadioButton typeCarre = new JRadioButton("Carré");
         JRadioButton typeTriangle = new JRadioButton("Triangle");
         this.setFloatable(true);
 		this.setRollover(true);
 		this.setBackground(Color.lightGray);
+        typeAucun.addActionListener(this);
+		typeAucun.setActionCommand("Aucun");
 		typeRond.addActionListener(this);
 		typeRond.setActionCommand("Rond");
 		typeCarre.addActionListener(this);
 		typeCarre.setActionCommand("Carre");
 		typeTriangle.addActionListener(this);
 		typeTriangle.setActionCommand("Triangle");
+        typeSommet.add(typeAucun);
         typeSommet.add(typeRond);
 		typeSommet.add(typeCarre);
 		typeSommet.add(typeTriangle);
-		this.add(typeRond);
+		this.add(typeAucun);
+        this.add(typeRond);
 		this.add(typeCarre);
 		this.add(typeTriangle);
         JButton suprArc=new JButton("Supprimer Arc");
@@ -39,7 +44,7 @@ public class BarreOutils extends JToolBar implements ActionListener{
     }
     public void actionPerformed(ActionEvent ae) {
         String action = ae.getActionCommand();
-        if (action.equals("Rond")||action.equals("Carre")||action.equals("Triangle")) {
+        if (action.equals("Rond")||action.equals("Carre")||action.equals("Triangle")||action.equals("Aucun")) {
             this.d.setType(action);
         }
         else if (action.equals("SuprArc")){
