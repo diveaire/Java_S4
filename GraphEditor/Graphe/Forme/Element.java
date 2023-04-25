6fff6b15c757196b1f596c3fcb0890354e0ad83c
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-25 20:48:19 +0200

    Update Element.java

--- GraphEditor/Graphe/Forme/Element.java
+++ GraphEditor/Graphe/Forme/Element.java
@@ -1,12 +1,11 @@
-package Graphe.Forme;
-
-
+import javax.swing.*;
 import java.awt.*;
 
 public abstract class Element{
     private String nom;
+    private static int nomDisplaySize=15;
     private Color couleur;
-    private Element(String nom){
+    Element(String nom){
         this.nom=nom;
     }
     Element(String nom,Color c){
@@ -16,11 +15,17 @@ public abstract class Element{
     public void setNom(String n){
         this.nom=n;
     }
+    static public void setNomDisplaySize(int n){
+        nomDisplaySize=n;
+    }
     public void setCouleur(Color a){
         this.couleur=a;
     }
     public String getNom(){
         return this.nom;
+    }
+    static public int getNomDisplaySize(){
+        return nomDisplaySize;
     }
     public Color getCouleur(){
         return this.couleur;
