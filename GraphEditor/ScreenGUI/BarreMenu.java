9bc278eed78028b4fbf674d69a7085a81f3254e5
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-29 12:54:44 +0200

    Update BarreMenu.java

--- GraphEditor/ScreenGUI/BarreMenu.java
+++ GraphEditor/ScreenGUI/BarreMenu.java
@@ -145,7 +145,7 @@ public class BarreMenu extends JMenuBar implements ActionListener{
             File fichier = chooser.getSelectedFile();
             if(fichier.exists()) {
                 try {
-                    screen.d.setGraphe(screen.d.getGraphe().deserialize(fichier.getName()));
+                    screen.d.setGraphe(screen.d.getGraphe().deserialize(fichier.getAbsolutePath()));
                     screen.d.repaint();
                 } catch(Exception ex) {
                     ex.printStackTrace();
