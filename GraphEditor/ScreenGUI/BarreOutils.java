e456b455d777ebdc7f688fae3ca9d1a8e598a85e
Emjoh60 <125392656+Emjoh60@users.noreply.github.com>
2023-04-29 17:41:43 +0200

    Add files via upload

--- GraphEditor/ScreenGUI/BarreOutils.java
+++ GraphEditor/ScreenGUI/BarreOutils.java
@@ -1,58 +1,107 @@
 package ScreenGUI;
 
-
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.event.*;
 
 public class BarreOutils extends JToolBar implements ActionListener{
-    protected Dessin d;
+    /* Attribut */
+    private Dessin d; // Le dessin sur lequel effectué les modifications
+    private JRadioButton typeAucun,typeRond,typeCarre,typeTriangle,typeArete,typeOriente;
+    private ButtonGroup typeSommet,typeArc;
+    private JButton convRond, convCarre, convTriangle,suprArc;
+    private JPanel selType,selArc,convLab,supLab;
+    /* Constructeur */
     public BarreOutils(String nom,Dessin draw){
         super(nom);
         this.d=draw;
-
-        ButtonGroup typeSommet = new ButtonGroup();
-        JRadioButton typeAucun = new JRadioButton("Aucun");
-		JRadioButton typeRond = new JRadioButton("Rond");
-        JRadioButton typeCarre = new JRadioButton("Carré");
-        JRadioButton typeTriangle = new JRadioButton("Triangle");
+        /* Initialisation des propriétés de la barre d'outils */
         this.setFloatable(true);
 		this.setRollover(true);
-		this.setBackground(Color.lightGray);
-        typeAucun.addActionListener(this);
-		typeAucun.setActionCommand("Aucun");
-		typeRond.addActionListener(this);
-		typeRond.setActionCommand("Rond");
-		typeCarre.addActionListener(this);
-		typeCarre.setActionCommand("Carre");
-		typeTriangle.addActionListener(this);
-		typeTriangle.setActionCommand("Triangle");
-        typeSommet.add(typeAucun);
-        typeSommet.add(typeRond);
-		typeSommet.add(typeCarre);
-		typeSommet.add(typeTriangle);
-		this.add(typeAucun);
-        this.add(typeRond);
-		this.add(typeCarre);
-		this.add(typeTriangle);
+		this.setBackground(Color.WHITE);
+        Border appBorder = BorderFactory.createLineBorder(Color.DARK_GRAY);
 
-        JButton suprArc=new JButton("Supprimer Arc");
-        suprArc.addActionListener(this);
-		suprArc.setActionCommand("SuprArc");
-        this.add(suprArc);
+        /* Création des boutons de sélection du type de sommet */
+        this.selType = new JPanel();
+        this.selType.setBorder(appBorder);
+        this.selType.setBackground(Color.CYAN);
+        this.selType.setLayout(new GridLayout(5,1));
+        this.selType.add(new JLabel("TYPE DE SOMMET :"));
+        this.typeSommet = new ButtonGroup();
+        this.typeAucun = new JRadioButton("Aucun");
+		this.typeRond = new JRadioButton("Rond");
+        this.typeCarre = new JRadioButton("Carré");
+        this.typeTriangle = new JRadioButton("Triangle");
+        this.typeAucun.addActionListener(this);
+		this.typeAucun.setActionCommand("Aucun");
+		this.typeRond.addActionListener(this);
+		this.typeRond.setActionCommand("Rond");
+		this.typeCarre.addActionListener(this);
+		this.typeCarre.setActionCommand("Carre");
+		this.typeTriangle.addActionListener(this);
+		this.typeTriangle.setActionCommand("Triangle");
+        this.typeSommet.add(this.typeAucun);
+        this.typeSommet.add(this.typeRond);
+		this.typeSommet.add(this.typeCarre);
+		this.typeSommet.add(this.typeTriangle);
+		this.selType.add(this.typeAucun);
+        this.selType.add(this.typeRond);
+		this.selType.add(this.typeCarre);
+		this.selType.add(this.typeTriangle);
+        this.add(this.selType);
 
-        ButtonGroup typeArc = new ButtonGroup();
-        JRadioButton typeArete = new JRadioButton("Arete");
-		JRadioButton typeOriente = new JRadioButton("Arc");
-        typeArete.addActionListener(this);
-		typeArete.setActionCommand("Arete");
-		typeOriente.addActionListener(this);
-		typeOriente.setActionCommand("Arc");
-        typeArc.add(typeArete);
-        typeArc.add(typeOriente);
-        this.add(typeArete);
-        this.add(typeOriente);
+        /* Création des boutons de conversion des sommets */
+        this.convLab = new JPanel();
+        this.convLab.setBorder(appBorder);
+        this.convLab.setBackground(Color.CYAN);
+        this.convLab.setLayout(new GridLayout(4,1));
+        this.convLab.add(new JLabel("  CHANGER LE TYPE DU SOMMET :"));
+        this.convRond=new JButton("Convertir en Rond");
+        this.convRond.addActionListener(this);
+		this.convRond.setActionCommand("convR");
+        this.convLab.add(convRond);
+
+        this.convCarre=new JButton("Convertir en Carre");
+        this.convCarre.addActionListener(this);
+		this.convCarre.setActionCommand("convC");
+        this.convLab.add(convCarre);
+
+        this.convTriangle=new JButton("Convertir en Triangle");
+        this.convTriangle.addActionListener(this);
+		this.convTriangle.setActionCommand("convT");
+        this.convLab.add(convTriangle);
+        this.add(this.convLab);
+
+        /* Création des boutons de sélection des arcs */
+        this.selArc = new JPanel();
+        this.selArc.setBorder(appBorder);
+        this.selArc.setBackground(Color.CYAN);
+        this.selArc.setLayout(new GridLayout(3,1));
+        this.selArc.add(new JLabel("  TYPE D'ARC :"));
+        this.typeArc = new ButtonGroup();
+        this.typeArete = new JRadioButton("Arete");
+		this.typeOriente = new JRadioButton("Arc");
+        this.typeArete.addActionListener(this);
+		this.typeArete.setActionCommand("Arete");
+		this.typeOriente.addActionListener(this);
+		this.typeOriente.setActionCommand("Arc");
+        this.typeArc.add(this.typeArete);
+        this.typeArc.add(this.typeOriente);
+        this.selArc.add(this.typeArete);
+        this.selArc.add(this.typeOriente);
+        this.add(this.selArc);
+        
+        /* Création du bouton de suppression d'un arc */
+        this.supLab = new JPanel();
+        this.supLab.setBorder(appBorder);
+        this.suprArc=new JButton("Supprimer Arc");
+        this.suprArc.addActionListener(this);
+		this.suprArc.setActionCommand("SuprArc");
+        this.supLab.add(suprArc);
+        this.add(this.supLab);
     }
+    /* Méthode */
     public Dessin getDessin(){
         return this.d;
     }
@@ -72,6 +121,30 @@ public class BarreOutils extends JToolBar implements ActionListener{
         }
         else if (action.equals("SuprArc")){
             this.d.delSelArc();
+        }
+        else if (action.equals("convR")){
+            if(this.d.getSelSom()!=null){
+                this.d.getGraphe().convertRond(this.d.getSelSom());
+                this.d.setSelSom(null);
+                this.d.setPrevSelSom(null);
+                this.d.repaint();
+            }
+        }
+        else if (action.equals("convC")){
+            if(this.d.getSelSom()!=null){
+                this.d.getGraphe().convertCarre(this.d.getSelSom());
+                this.d.setSelSom(null);
+                this.d.setPrevSelSom(null);
+                this.d.repaint();
+            }
+        }
+        else if (action.equals("convT")){
+            if(this.d.getSelSom()!=null){
+                this.d.getGraphe().convertTriangle(this.d.getSelSom());
+                this.d.setSelSom(null);
+                this.d.setPrevSelSom(null);
+                this.d.repaint();
+            }
         }
     }
 }
